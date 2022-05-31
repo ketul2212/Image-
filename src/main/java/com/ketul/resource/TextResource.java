aad02b17e48e858403627c858f6b3c2e950aad60
ketul2212 <pketul2212@gmail.com>
2022-05-31 10:29:45 +0530

    Some new Features and get dynamic images to user and accept all images

--- src/main/java/com/ketul/resource/TextResource.java
+++ src/main/java/com/ketul/resource/TextResource.java
@@ -1,33 +1,33 @@
 package com.ketul.resource;
 
+
+import com.ketul.service.TextService;
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.apache.commons.io.FilenameUtils;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.time.LocalDateTime;
 
 import static org.springframework.http.ResponseEntity.notFound;
 
 @RestController
+
 public class TextResource {
+    @Autowired
+    private TextService textService;
 
-    @GetMapping("/")
-    public ResponseEntity<Object> getTextIntoImage() {
-        File file = new File("hello.png");
-        System.out.println(file.exists());
-        ITesseract instance = new Tesseract();
-        instance.setDatapath("tessdata");
-
-        try {
-            String result = instance.doOCR(file);
-            System.out.println(result);
-            return ResponseEntity.ok(result);
-        } catch (TesseractException e) {
-            System.err.println(e.getMessage());
-            return notFound().build();
-        }
+    @PostMapping("/")
+    public ResponseEntity<?> getTextIntoImage(@RequestParam("file") MultipartFile multipartFile) {
+        return textService.getTextIntoImage(multipartFile);
     }
 }
---
+++ src/main/java/com/ketul/service/TextService.java
@@ -0,0 +1,53 @@
+package com.ketul.service;
+
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Objects;
+
+@Service
+public class TextService {
+    public ResponseEntity<?> getTextIntoImage(MultipartFile multipartFile) {
+
+        if(multipartFile.isEmpty())
+            return ResponseEntity.ok("File is empty please select a file");
+
+        File file = new File("uploads/output.png");
+
+        ITesseract iTesseract = new Tesseract();
+        iTesseract.setDatapath("tessdata");
+
+
+
+        try (OutputStream os = new FileOutputStream(file)) {
+            os.write(multipartFile.getBytes());
+
+            String extension = FilenameUtils.getExtension(multipartFile.getOriginalFilename());
+            if(Objects.requireNonNull(extension).equalsIgnoreCase("jpg")) {
+                BufferedImage image = ImageIO.read(file);
+                ImageIO.write(image, "png", file);
+            }
+
+        } catch (IOException e) {
+            return ResponseEntity.ok("some error occur to write file into ");
+        }
+
+        String result;
+        try {
+            result = iTesseract.doOCR(file);
+        } catch (TesseractException e) {
+            return ResponseEntity.ok("some error occur to convert images to Text");
+        }
+
+        System.out.println(result);
+        return ResponseEntity.ok(result);
+    }
+}
